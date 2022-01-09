d92c75c20fd9e4428f7c2f2c8a8c49fdad29cb4c
Mahmut Furkan ÇÖKEK <44062559+mfcokek@users.noreply.github.com>
2022-01-09 20:25:24 +0300

    Update Dashboard.js

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 export default function Dashboard() {
+    // var olan giriş işlemi localde olduğu için isim çekilir 
     const user_email = localStorage.getItem("@user_email")
     return (
         <div>
             <p>Hoşgeldin {user_email}</p>
+            //çıkış işlemine yönlendirilir
             <a href="/logout">çıkış yap</a>
         </div>
     )
