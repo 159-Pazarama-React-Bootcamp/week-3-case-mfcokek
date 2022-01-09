d9acde141f399640bc1e6f8950c4c4c4c5e3640d
Mahmut Furkan ÇÖKEK <44062559+mfcokek@users.noreply.github.com>
2022-01-09 20:22:15 +0300

    yorum satırı

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Logout() {
+    // var olan giriş işlemini sonlandırır
     localStorage.removeItem("@user_email")
     window.location = "/"
     return null
